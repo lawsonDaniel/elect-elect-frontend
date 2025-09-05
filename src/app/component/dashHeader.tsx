2b6f3f1d9213f60bd6f2fc5474b0e753b45decae
EnochScript <enochfolorunso2018@gmail.com>
2025-09-05 09:18:58 +0100

    removed dashboard searchbar

--- src/app/component/dashHeader.tsx
+++ src/app/component/dashHeader.tsx
@@ -36,34 +36,8 @@ export default function DashHeader({ isMobileOpen, setIsMobileOpen }: DashboardH
           <Menu size={40} className='bg-white p-2 border text-[#6B7280] rounded-md border-[#6B7280] dark:bg-[#070E12] dark:border-[#EDF3F8]' />
         </div>
         
-        {/* Mobile: Show only search icon */}
-        <div className="sm:hidden flex-shrink-0">
-          <button className="w-10 h-10 flex items-center justify-center bg-white border border-[#6B7280] rounded-md dark:bg-[#070E12] dark:border-[#EDF3F8]">
-            <Image
-              src='/search.png'
-              width={20}
-              height={20}
-              alt="search"
-              className="dark:invert"
-            />
-          </button>
-        </div>
-
-        {/* Desktop: Show full search bar */}
-        <div className="hidden sm:block relative w-full max-w-sm md:max-w-md lg:w-[31.5rem] lg:max-w-none">
-          <Image
-            src='/search.png'
-            width={24}
-            height={24}
-            alt="search"
-            className="absolute top-1/2 -translate-y-1/2 left-3 flex-shrink-0 dark:invert"
-          />
-          <input
-            type="search"
-            placeholder="Search for Anything"
-            className="w-full text-[#6B7280] pl-10 pr-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-black text-sm sm:text-base dark:bg-[#070E12] dark:border-[#EDF3F8] dark:text-[#EDF3F8] dark:focus:ring-[#EDF3F8]"
-          />
-        </div>
+       
+        
       </div>
 
       {/* Right side: toggle + notification */}
